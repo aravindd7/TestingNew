49b785ef11d6decd3e3af519a5b341f631c6fe72
aravindd7 <ak154761@gmail.com>
2022-07-30 13:54:43 +0530

    content ++

--- KisanNetwork/frontenddesignchallenge/src/components/Header.js
+++ KisanNetwork/frontenddesignchallenge/src/components/Header.js
@@ -1,9 +1,12 @@
-import {Box, Typography} from '@mui/material';
+import { Box, Typography } from "@mui/material";
 
 function Header() {
   return (
-    <Box>
-      <Typography>HEader</Typography>
+    <Box sx={{height: "300px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <Typography>
+        Let us keep that promise - that American promise - and in the words of
+        Scripture hold firmly, without wavering, to the hope that we confess.
+      </Typography>
     </Box>
   );
 }
--- KisanNetwork/frontenddesignchallenge/src/components/Landing.js
+++ KisanNetwork/frontenddesignchallenge/src/components/Landing.js
@@ -1,9 +1,37 @@
 import {Box, Typography} from '@mui/material';
-
+import Menu from './Menu';
+import TextInputs from './TextInputs';
 function Landing() {
   return (
-    <Box>
-      <Typography>Landing</Typography>
+    <Box sx={{ display: "flex", flexDirection: "row"}}>
+      <Box sx={{ display: "flex", flexDirection: "column"}}>
+        <Typography>Justo Vulputate Tortor Sem</Typography>
+        <Typography>There is not a Black America and a White America and Latino America and Asian America - there's the United States of America. Born into poverty? Pull yourself up by your own bootstraps - even if you don't have boots.</Typography>
+        <Typography>In the following century, men and women of faith waded into the battles over prison reform and temperance, public education and women's rights - and above all, abolition. We shouldn't use the obstacles we face as an excuse for cynicism. I've seen it in Illinois, when we provided health care to more children and moved more families from welfare to work.</Typography>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column"}}>
+          <Menu />
+          <Box>
+            <Box sx={{ display: "flex", flexDirection: "row"}}>
+                <Typography>Name</Typography>
+                <Typography>Nomen Nescio</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row"}}>
+                <Typography>Email</Typography>
+                <Typography>nomen@example.com</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row"}}>
+                <Typography>Phone</Typography>
+                <Typography>+852 555 555</Typography>
+            </Box>
+          </Box>
+          <Box>
+             <Typography>
+                Send me a message
+             </Typography>
+             <TextInputs />
+          </Box>
+      </Box>
     </Box>
   );
 }
